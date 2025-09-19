82b85a1c237588f58ed6949de583137fe83dc7a8
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-09-19 13:27:32 +0200

    feat: option to plot abs and real for poli

--- main.py
+++ main.py
@@ -345,7 +345,13 @@ def simulate(coefficients = [0, 1], n = 5, asin_degree = 5):
     return sv
 
 
-def test_poly(coefficients: list[float], n: int = 5, asin_degrees: list[int] = [5]):
+def test_poly(
+    coefficients: list[float],
+    n: int = 5,
+    asin_degrees: list[int] = [5],
+    plot_real: bool = False,
+    plot_abs: bool = False,
+):
     svs = [ simulate(coefficients, n, d) for d in asin_degrees ]
 
     Nd = len(asin_degrees)
@@ -361,12 +367,14 @@ def test_poly(coefficients: list[float], n: int = 5, asin_degrees: list[int] = [
         clr = cm(Nd - 1 - i)
         d = asin_degrees[i]
         x, psi0, psi1 = interpret_sv(svs[i])
-        ax0.plot(x, np.real(psi0), label=f"Re:{d}", c=clr, ls=":")
         ax0.plot(x, np.imag(psi0), label=f"Im:{d}", c=clr)
-        # ax0.plot(x, np.abs(psi0), ls="--", c="gray", label=f"abs:{d}")
-        ax1.plot(x, np.real(psi1), label=f"Re:{d}", c=clr, ls=":")
         ax1.plot(x, np.imag(psi1), label=f"Im:{d}", c=clr)
-        # ax1.plot(x, np.abs(psi1), ls="--", c="gray", label=f"abs:{d}")
+        if plot_real:
+            ax0.plot(x, np.real(psi0), label=f"Re:{d}", c=clr, ls=":")
+            ax1.plot(x, np.real(psi1), label=f"Re:{d}", c=clr, ls=":")
+        if plot_abs:
+            ax0.plot(x, np.abs(psi0), label=f"abs:{d}", c=clr, ls="--")
+            ax1.plot(x, np.abs(psi1), label=f"abs:{d}", c=clr, ls="--")
     ax0.legend()
     ax1.legend()
     plt.show()
@@ -394,9 +402,11 @@ if __name__ == "__main__":
 
     # encodinf of polynomial
     ep_parser = sub.add_parser("poly", help="tests the block encoding of P(x)")
+    ep_parser.add_argument("coef", nargs='+', type=float, help="coefficients of the polynomial")
+    ep_parser.add_argument("-r", "--real", action="store_true", help="plot the real part")
+    ep_parser.add_argument("-a", "--abs", action="store_true", help="plot the absolute value")
     ep_parser.add_argument("-n", type=int, default=5, help="Number of encoding qubits")
     ep_parser.add_argument("-d", "--asin-degree", nargs="*", default=[5], type=int, help="degree(s) of the polynomial approximating arcsin")
-    ep_parser.add_argument("-c", "--coef", nargs='+', type=float, required=True, help="coefficients of the polynomial")
 
 
     ns = parser.parse_args()
@@ -409,7 +419,7 @@ if __name__ == "__main__":
     elif ns.cmd == "asin":
         test_asin(ns.degree, not ns.noplot, ns.real, ns.npts)
     elif ns.cmd == "poly":
-        test_poly(ns.coef, ns.n, ns.asin_degree)
+        test_poly(ns.coef, ns.n, ns.asin_degree, ns.real, ns.abs)
 
 
 
